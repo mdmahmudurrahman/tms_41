e2512a7014778377b946765a08aba16372474751
gittiggit <contactwithmahmud@gmail.com>
2015-12-03 17:11:34 +0600

    Commited in the 47122_user_authentication branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence:   true,
             format:     { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 5 }, allow_blank: true
+  #has_secure_password
+ # validates :password, presence: true, length: { minimum: 5 }, allow_blank: true
 
   has_many :user_courses
   has_many :courses, through: :user_courses
